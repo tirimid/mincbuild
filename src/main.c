14f56dd809b87ea9ad8168f411fa242a96277002
tirimid <q44q44q44q44@gmail.com>
2023-07-25 09:03:26 +0400

    fixed error code -1 to 1

--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@ main(int argc, char const *argv[])
 {
 	if (argc > 2) {
 		printf("usage: %s [build config file]\n", argv[0]);
-		return -1;
+		return 1;
 	}
 	
 	struct conf conf = conf_from_file(argc == 2 ? argv[1] : DEFAULT_CONF_FILE);
